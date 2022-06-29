044d6a575c2e33b1794ca7a00ff587d01d53e3fb
chigov2 <chigov@gmail.com>
2022-06-29 15:51:17 +0300

    35 begin

--- src/main/java/aop/aspects/UniversityLoggingAspect.java
+++ src/main/java/aop/aspects/UniversityLoggingAspect.java
@@ -1,10 +1,7 @@
 package aop.aspects;
 
 import aop.Student;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -31,8 +28,13 @@ public class UniversityLoggingAspect {
 //                "logging getting list of students after method getStudents");
 //        System.out.println("------------------------------------------------");
 //    }
-    @AfterThrowing(pointcut = "execution(* getStudents())", throwing = "exception")
-    public void afterThrowingError(Throwable exception){
-        System.out.println("Логируем выброс исключения " + exception);
+
+//    @AfterThrowing(pointcut = "execution(* getStudents())", throwing = "exception")
+//    public void afterThrowingError(Throwable exception){
+//        System.out.println("Логируем выброс исключения " + exception);
+//    }
+    @After("execution(* getStudents())")
+    public void afterGetStudents(){
+        System.out.println("afterGetStudents()");
     }
 }
